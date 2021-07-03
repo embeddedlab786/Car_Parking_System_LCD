488d8cf2f8a4d3ec8ef66b7a8b86cccc89f8956e
Muhammad Ansar <57707946+embeddedlab786@users.noreply.github.com>
2021-07-03 16:13:14 -0700

    Update Car_Parking_System_LCD.ino

--- Car_Parking_System_LCD.ino
+++ Car_Parking_System_LCD.ino
@@ -1,7 +1,7 @@
 #include <Servo.h> //includes the servo library
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 Servo myservo;
 
